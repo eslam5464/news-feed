300bd125f1ea5e5dc493d9f3893ab9fa425f93f9
Eslam <41278977+eslam5464@users.noreply.github.com>
2024-10-11 04:21:25 +0300

    add likes in post route and add like deps

---
+++ backend/app/deps/like.py
@@ -0,0 +1,58 @@
+import ast
+from datetime import datetime, UTC
+from typing import Any
+
+from flask import jsonify
+from pydantic import ValidationError
+from starlette import status
+
+from app import repos, schemas
+from app.db import get_db
+
+
+def like_post(post_id: int, like_data):
+    conn = get_db()
+    post_db = repos.Post(conn).get_one(post_id)
+
+    if not post_db:
+        return jsonify({"message": "Post not found"}), status.HTTP_404_NOT_FOUND
+
+    data_decoded: dict[str, Any] = ast.literal_eval(like_data.decode("utf-8"))
+    data_decoded["post_id"] = post_id
+
+    try:
+        like_in = schemas.LikeCreateIn(**data_decoded)
+    except ValidationError as e:
+        return jsonify({'error': str(e)}), status.HTTP_400_BAD_REQUEST
+
+    likes_db = repos.Like(conn).get_all_by_user_id(like_in.user_id)
+    all_liked_posts = [like_entry.post_id for like_entry in likes_db]
+
+    if post_id in all_liked_posts:
+        return jsonify({'message': 'Post already liked'}), status.HTTP_400_BAD_REQUEST
+
+    like_new = schemas.LikeCreate(
+        **like_in.model_dump(),
+        creation_timestamp=datetime.now(UTC).replace(tzinfo=None)
+    )
+    like_id = repos.Like(conn).create(like_new)
+
+    return jsonify({'message': 'Liked the post', 'id': like_id}), status.HTTP_201_CREATED
+
+
+def get_all_likes(post_id: int):
+    conn = get_db()
+    post_db = repos.Post(conn).get_one(post_id)
+
+    if not post_db:
+        return jsonify({"message": "Post not found"}), status.HTTP_404_NOT_FOUND
+
+    likes_db = repos.Like(conn).get_all_by_post_id(post_id)
+
+    all_likes = [
+        user_entry.model_dump()
+        for user_entry
+        in likes_db
+    ]
+
+    return all_likes
--- backend/app/routes/post.py
+++ backend/app/routes/post.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request
 
+from app.deps.like import like_post, get_all_likes
 from app.deps.post import create_post, get_post
 
 post = Blueprint('post', __name__)
@@ -8,6 +9,16 @@ post = Blueprint('post', __name__)
 @post.route("/post", methods=['POST'])
 def publish_post():
     return create_post(request.data)
+
+
+@post.route("post/<int:post_id>/like", methods=['POST'])
+def perform_like(post_id: int):
+    return like_post(post_id, request.data)
+
+
+@post.route("post/<int:post_id>/likes", methods=['GET'])
+def get_all_likes_for_post(post_id: int):
+    return get_all_likes(post_id)
 
 
 @post.route("/post/<int:post_id>", methods=['GET'])
