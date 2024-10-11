03b8a076e39754418128f02640260e7957b27924
Eslam <41278977+eslam5464@users.noreply.github.com>
2024-10-11 15:21:41 +0300

    update comment repos and add routes to comment in post route

--- backend/app/repos/comment.py
+++ backend/app/repos/comment.py
@@ -24,14 +24,17 @@ class Comment:
 
         return user_id
 
-    def get_one(self,comment_id: int):
+    def get_one(self, comment_id: int):
         cursor = self.conn.cursor()
 
-        query = f"SELECT * FROM comment WHERE comment_id = {comment_id}"
+        query = f"SELECT * FROM comment WHERE id = {comment_id}"
         cursor.execute(query)
         comment_db = cursor.fetchone()
 
         cursor.close()
+
+        if not comment_db:
+            return None
 
         return schemas.Comment(
             id=comment_db[0],
@@ -40,3 +43,35 @@ class Comment:
             content=comment_db[3],
             creation_timestamp=comment_db[4],
         )
+
+    def get_all_by_post_id(self, post_id: int):
+        cursor = self.conn.cursor()
+
+        query = f"SELECT * FROM comment WHERE post_id = {post_id}"
+        cursor.execute(query)
+        comments_db = cursor.fetchall()
+
+        cursor.close()
+
+        return [
+            schemas.Comment(
+                id=comment_entry[0],
+                user_id=comment_entry[1],
+                post_id=comment_entry[2],
+                content=comment_entry[3],
+                creation_timestamp=comment_entry[4],
+            )
+            for comment_entry
+            in comments_db
+        ]
+
+    def delete(self,comment_id:int):
+        cursor = self.conn.cursor()
+
+        query = f"DELETE FROM comment WHERE id = {comment_id}"
+        cursor.execute(query)
+
+        self.conn.commit()
+        cursor.close()
+
+        return None
--- backend/app/routes/post.py
+++ backend/app/routes/post.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request
 
+from app.deps.comment import get_all_comments, add_comment, remove_comment
 from app.deps.like import like_post, get_all_likes
 from app.deps.post import create_post, get_post
 
@@ -9,6 +10,11 @@ post = Blueprint('post', __name__)
 @post.route("/post", methods=['POST'])
 def publish_post():
     return create_post(request.data)
+
+
+@post.route("/post/<int:post_id>", methods=['GET'])
+def retrieve_post(post_id: int):
+    return get_post(post_id).model_dump()
 
 
 @post.route("post/<int:post_id>/like", methods=['POST'])
@@ -21,6 +27,16 @@ def get_all_likes_for_post(post_id: int):
     return get_all_likes(post_id)
 
 
-@post.route("/post/<int:post_id>", methods=['GET'])
-def retrieve_post(post_id: int):
-    return get_post(post_id)
+@post.route("post/<int:post_id>/comments", methods=['GET'])
+def get_all_comments_for_post(post_id: int):
+    return get_all_comments(post_id)
+
+
+@post.route("post/<int:post_id>/comment", methods=['POST'])
+def add_comment_to_post(post_id: int):
+    return add_comment(post_id, request.json)
+
+
+@post.route("post/<int:post_id>/comment/<int:comment_id>", methods=['DELETE'])
+def delete_comment_from_post(post_id: int, comment_id: int):
+    return remove_comment(post_id, comment_id)
